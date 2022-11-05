408a40c0589bdd4d397d91b983a66c641119895c
sarushanc <sarushan@jwareautomation.com>
2022-11-05 23:23:23 +0530

    add user

---
+++ lib/screens/addUsers.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AddUser extends StatefulWidget {
+  const AddUser({super.key});
+
+  @override
+  State<AddUser> createState() => _AddUserState();
+}
+
+class _AddUserState extends State<AddUser> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add New User"),
+      ),
+      body: const Center(
+        child: Text("Add User"),
+      ),
+    );
+  }
+}
