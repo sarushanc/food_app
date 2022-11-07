76ccba32b6ec34e7aa71b702904ece53c1806c5f
sarushanc <sarushan@jwareautomation.com>
2022-11-07 22:31:01 +0530

    user

--- lib/screens/addUsers.dart
+++ lib/screens/addUsers.dart
@@ -8,6 +8,9 @@ class AddUser extends StatefulWidget {
 }
 
 class _AddUserState extends State<AddUser> {
+  final _userNameController = TextEditingController();
+  final _userContactController = TextEditingController();
+  final _userDescriptionController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,16 +21,78 @@ class _AddUserState extends State<AddUser> {
           child: Container(
             padding: const EdgeInsets.all(16.0),
             child: Column(
-              children: const [
-                Text('Add New User',
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Add New User',
                   style: TextStyle(
                       fontSize: 20,
                       color: Colors.teal,
                       fontWeight: FontWeight.w500),
-              ),
-            ]),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  controller: _userNameController,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter Name',
+                      labelText: 'Name'),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  controller: _userContactController,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter Contact',
+                      labelText: 'Contact'),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  controller: _userDescriptionController,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter Description',
+                      labelText: 'Description'),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.teal,
+                          textStyle: const TextStyle(fontSize: 15)),
+                      onPressed: () {},
+                      child: const Text('SAVE'),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.red,
+                          textStyle: const TextStyle(fontSize: 15)),
+                      onPressed: () {
+                        _userNameController.text = '';
+                        _userContactController.text = '';
+                        _userDescriptionController.text = '';
+                      },
+                      child: const Text('CLEAR'),
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
-        )
-      );
+        ));
   }
 }
