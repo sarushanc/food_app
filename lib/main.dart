53bb71491aa023ae95fe47f1de1ce1ee96efeb1f
sarushanc <sarushan@jwareautomation.com>
2022-11-16 17:41:35 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'First App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.teal,
